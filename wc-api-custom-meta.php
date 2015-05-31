a8528292b8e14492e32edefdfe0e6dc9c64c877a
Jason Judge <jason.judge@consil.co.uk>
2015-05-31 11:21:52 +0100

    Move functions to static class.

--- wc-api-custom-meta.php
+++ wc-api-custom-meta.php
@@ -1,102 +1,157 @@
 <?php
 /*
-Plugin Name: WC API Custom Metafields
-Plugin URI:  https://github.com/academe/TBC
-Description: Allows custom meta fields to be added to products when creating or updating.
-Version:     0.5.1
+Plugin Name: WC API Custom Meta
+Plugin URI:  hhttps://github.com/judgej/wc-api-custom-meta
+Description: Allows access to custom meta fields on products through the API.
+Version:     0.6.0
 Author:      Jason Judge
 Author URI:  http://academe.co.uk
 */
 
-// Want to hook into woocommerce_api_process_product_meta_{product_type} for all product types.
+/**
+ * Everything is static at present.
+ * We may go singletone route in the future if there is some state
+ * to handle, for example if the list of protected fields can be
+ * modified - perhaps through a filter hook - at the start.
+ */
 
-add_action('woocommerce_api_process_product_meta_simple', 'academe_wc_api_custom_meta', 10, 2);
-add_action('woocommerce_api_process_product_meta_variable', 'academe_wc_api_custom_meta', 10, 2);
-add_action('woocommerce_api_process_product_grouped', 'academe_wc_api_custom_meta', 10, 2);
-add_action('woocommerce_api_process_product_external', 'academe_wc_api_custom_meta', 10, 2);
+class Academe_Wc_Api_Custom_Meta
+{
+    // Meta fields we want to protect, due to them being already handled
+    // by the WC API.
+    // To view or change these fields, go through the appropriate API.
 
-function academe_wc_api_custom_meta($id, $data) {
-    if (!empty($data['custom_meta']) && is_array($data['custom_meta'])) {
-        foreach($data['custom_meta'] as $field_name => $field_value) {
-            update_post_meta($id, $field_name, wc_clean($field_value));
+    protected static $protected_fields = array(
+        // A few WP internal fields should not be exposed.
+        '_edit_lock',
+        '_edit_last',
+        // All these meta fields are already present in the
+        // product_data in some form.
+        '_visibility',
+        '_stock_status',
+        'total_sales',
+        '_downloadable',
+        '_virtual',
+        '_regular_price',
+        '_sale_price',
+        '_purchase_note',
+        '_featured',
+        '_weight',
+        '_length',
+        '_width',
+        '_height',
+        '_sku',
+        '_product_attributes',
+        '_price',
+        '_sold_individually',
+        '_manage_stock',
+        '_backorders',
+        '_stock',
+        '_upsell_ids',
+        '_crosssell_ids',
+        '_product_image_gallery',
+        '_sale_price_dates_from',
+        '_sale_price_dates_to',
+    );
+
+    protected static $product_type = array(
+        'meta_simple',
+        'meta_variable',
+        'grouped',
+        'external',
+    );
+
+    /**
+     * Initialise the hooks at plugin initialisation.
+     */
+    public static function initialize()
+    {
+        // GET product: add in meta field to results.
+        add_filter(
+            'woocommerce_api_product_response',
+            array('Academe_Wc_Api_Custom_Meta', 'fetchCustomMeta'),
+            10,
+            4
+        );
+
+        // Want to hook into woocommerce_api_process_product_meta_{product_type} for all product types.
+        foreach(static::$product_type as $product_type) {
+            add_action(
+                'woocommerce_api_process_product_' . $product_type,
+                array('Academe_Wc_Api_Custom_Meta', 'updateCustomMeta'),
+                10,
+                2
+            );
         }
     }
 
-    if (!empty($data['custom_meta']) && is_array($data['remove_custom_meta'])) {
-        foreach($data['remove_custom_meta'] as $key => $value) {
-            // If the key is numeric, then assume $value is the field name
-            // and all entries need to be deleted. Otherwise is is a specfic value
-            // of a named meta field that should be removed.
+    /**
+     * Fetch a product.
+     */
+    public static function fetchCustomMeta($product_data, $product, $fields, $server) {
+        // The admin and shop manager will have the capability "manage_woocommerce".
+        // We only want users with this capability to see additional product meta fields.
 
-            if (is_numeric($key)) {
-                delete_post_meta($id, $value);
-            } else {
-                delete_post_meta($id, $key, $value);
+        if (current_user_can('manage_woocommerce')) {
+            $product_id = $product->id;
+
+            $all_meta = get_post_meta($product_id);
+
+            // Filter out meta we don't want.
+            $all_meta = array_diff_key($all_meta, array_flip(static::$protected_fields));
+
+            // Unserialize the meta field data where necessary.
+            foreach($all_meta as $key => &$value) {
+                $value = maybe_unserialize(reset($value));
+            }
+            unset($value);
+
+            $meta = array($all_meta);
+
+            $product_data['meta'] = $meta;
+        }
+
+        return $product_data;
+    }
+
+    /**
+     * Update or create a product.
+     */
+    public static function updateCustomMeta($id, $data) {
+        // Create or update fields.
+        if (!empty($data['custom_meta']) && is_array($data['custom_meta'])) {
+            // Filter out protected fields.
+            $custom_meta = array_diff_key(
+                $data['custom_meta'],
+                array_flip(static::$protected_fields)
+            );
+
+            foreach($custom_meta as $field_name => $field_value) {
+                update_post_meta($id, $field_name, wc_clean($field_value));
+            }
+        }
+
+        // Remove meta fields.
+        if (!empty($data['remove_custom_meta']) && is_array($data['remove_custom_meta'])) {
+            // Filter out protected fields.
+            $remove_custom_meta = array_diff(
+                $data['remove_custom_meta'],
+                static::$protected_fields
+            );
+
+            foreach($remove_custom_meta as $key => $value) {
+                // If the key is numeric, then assume $value is the field name
+                // and all entries need to be deleted. Otherwise is is a specfic value
+                // of a named meta field that should be removed.
+
+                if (is_numeric($key)) {
+                    delete_post_meta($id, $value);
+                } else {
+                    delete_post_meta($id, $key, $value);
+                }
             }
         }
     }
 }
 
-
-// GET product through the API
-add_filter('woocommerce_api_product_response', 'academe_wc_api_get_meta', 10, 4);
-
-function academe_wc_api_get_meta($product_data, $product, $fields, $server) {
-    // The admin and shop manager will have the capability "manage_woocommerce".
-    // We only want users with this capability to see additional product meta fields.
-
-    if (current_user_can('manage_woocommerce')) {
-        $product_id = $product->id;
-
-        // Meta fields we don't want, due to them being already in the data.
-        $exclude = array(
-            // A few WP internal fields should not be exposed.
-            "_edit_lock",
-            "_edit_last",
-            // All these meta fields are already present in the product_data in some form.
-            "_visibility",
-            "_stock_status",
-            "total_sales",
-            "_downloadable",
-            "_virtual",
-            "_regular_price",
-            "_sale_price",
-            "_purchase_note",
-            "_featured",
-            "_weight",
-            "_length",
-            "_width",
-            "_height",
-            "_sku",
-            "_product_attributes",
-            "_price",
-            "_sold_individually",
-            "_manage_stock",
-            "_backorders",
-            "_stock",
-            "_upsell_ids",
-            "_crosssell_ids",
-            "_product_image_gallery",
-            // The sale dates are actually not directly in the normal data returned.
-            "_sale_price_dates_from",
-            "_sale_price_dates_to",
-        );
-
-        $all_meta = get_post_meta($product_id);
-
-        // Filter out meta we don't want.
-        $all_meta = array_diff_key($all_meta, array_flip($exclude));
-
-        // Unserialize the meta field data where necessary.
-        foreach($all_meta as $key => &$value) {
-            $value = maybe_unserialize(reset($value));
-        }
-        unset($value);
-
-        $meta = array($all_meta);
-
-        $product_data['meta'] = $meta;
-    }
-
-    return $product_data;
-}
+Academe_Wc_Api_Custom_Meta::initialize();
